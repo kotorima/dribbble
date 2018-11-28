b9e731dd4bb93a8da5f885b125d7f2b16bedd538
kotorima <kotorimas@gmail.com>
2018-11-28 14:38:01 +0300

    problem

--- js/pagination.js
+++ js/pagination.js
@@ -1,13 +1,49 @@
+function creatingPages(count){
+	for (let i = 1; i <= count; i++) {
+		if (count === 1) {
+			$('.pages').append('</li><li class="selected page">'+i+'</li>');
+		}
+		else if (i === 1) {
+			$('.pages').append('<li class="changeLeft"><i class="fa fa-angle-left" aria-hidden="true"></i></li><li class="selected page">'+i+'</li>');
+		}
+		else if(i === 6 && count > 8) {
+			$('.pages').append('<li class="disclose">...</li>');
+		}
+		else if(i > 6 && count > 8 && i != count) {
+			
+		}
+		else if(i === count){
+			$('.pages').append('<li class="page">'+i+'</li><li class="changeRight"><i class="fa fa-angle-right" aria-hidden="true"></i></li>');
+		}
+		else {
+			$('.pages').append('<li class="page">'+i+'</li>');
+		}
+	}
+}
+
 $(document).on("click", '.page', function(){
 	$('.page').removeClass('selected');
 	$(this).addClass('selected');
+	console.log('opo'+$(this).index());
+	let request = $.ajax({
+		type: "GET",
+		url: "http://gallery.dev.webant.ru/api/photos?new=true&page="+$(this).index()+"&limit=1",
+		dataType: "json",
+		cache: true,
+		beforeSend: loader()
+	});
+	request.done(function(response){
+	response.data.forEach(function(item){
+		if(item.new === true) {
+			$('main').append('<figure><img src="http://gallery.dev.webant.ru/media/'+item.image.contentUrl+'" alt="picture"><figcaption><h2>'+item.name+'</h2><p>'+item.description+'</p></figcaption></figure>');
+		}
+	});
+	});
 });
-
-
-let pages = $('.page');
+let pages = $('.pages');
 let page = $('.pages').find('.page.selected').index();
 let qualityPages = pages.length;
-console.log(qualityPages);
+console.log(pages);
 if (page === 1) { 
 	$('.changeLeft').addClass('inactive');
 }
@@ -15,6 +51,7 @@ if (page === 1) {
 
 
 $(document).on("click", '.changeLeft', function(){	
+	console.log('olol');
 	if(page > 1) {
 		$('.page').removeClass('selected');
 		page--;
--- js/sorting.js
+++ js/sorting.js
@@ -1,3 +1,5 @@
+sorting('#new');
+
 $('#popular').click(function(){
 	sorting('#popular');
 	
@@ -10,40 +12,44 @@ function sorting (elem) {
 	$('li').removeClass('active');
 	$(elem).attr('class', 'active');
 	$('main').empty();
+	let request;
+	if (elem === '#new') {
+		request = $.ajax({
+			type: "GET",
+			url: "http://gallery.dev.webant.ru/api/photos?new=true&page=1&limit=1",
+			dataType: "json",
+			cache: true,
+			beforeSend: loader()
+		});
+	}
+	else if(elem === '#popular'){
+		request = $.ajax({
+			type: "GET",
+			url: "http://gallery.dev.webant.ru/api/photos?popular=true&page=1&limit=1",
+			dataType: "json",
+			cache: true,
+			beforeSend: loader()
+		});
+	}
 	request.done(function(response){
 		response.data.forEach(function(item, i, arr){
-			if(elem === '#popular'){
-				if(item.popular === true) {
-					$('main').append('<figure><img src="http://gallery.dev.webant.ru/media/'+item.image.contentUrl+'" alt="picture"><figcaption><h2>'+item.name+'</h2><p>'+item.description+'</p></figcaption></figure>');
-				}
+			if(elem === '#popular' && item.popular === true) {
+				$('main').append('<figure><img src="http://gallery.dev.webant.ru/media/'+item.image.contentUrl+'" alt="picture">\
+					<figcaption><h2>'+item.name+'</h2><p>'+item.description+'</p></figcaption></figure>');
 			}
-			else if (elem === '#new') {
-				if(item.new === true) {
-					$('main').append('<figure><img src="http://gallery.dev.webant.ru/media/'+item.image.contentUrl+'" alt="picture"><figcaption><h2>'+item.name+'</h2><p>'+item.description+'</p></figcaption></figure>');
-				}
+			else if (elem === '#new' && item.new === true) {
+				$('main').append('<figure><img src="http://gallery.dev.webant.ru/media/'+item.image.contentUrl+'" alt="picture">\
+					<figcaption><h2>'+item.name+'</h2><p>'+item.description+'</p></figcaption></figure>');
 			}
 		});
+		creatingPages(response.countOfPages);
+	});
+
+	request.fail(function(){
+		$("main").hide();
+		$(".pages").hide();
+		$("body").append("<figure class='offline'><img src='image/NoInternet.png' alt='offline'><figcaption><h2>Oh shucks!</h2>\
+			<p>Slow or no internet connection.</p><p>Please check your internet settings</p></figcaption></figure>");
 	});
 }
 
-let request = $.ajax({
-	type: "GET",
-	url: "http://gallery.dev.webant.ru/api/photos?new=true&page=1&limit=1",
-	dataType: "json",
-	cache: true,
-	beforeSend: loader()
-});
-request.done(function(response){
-	response.data.forEach(function(item){
-		if(item.new === true) {
-			$('main').append('<figure><img src="http://gallery.dev.webant.ru/media/'+item.image.contentUrl+'" alt="picture"><figcaption><h2>'+item.name+'</h2><p>'+item.description+'</p></figcaption></figure>');
-		}
-	});
-	let count = response.countOfPages;
-	creatingPages(count);
-});
-request.fail(function(){
-	$("main").hide();
-	$(".pages").hide();
-	$("body").append("<figure class='offline'><img src='image/NoInternet.png' alt='offline'><figcaption><h2>Oh shucks!</h2><p>Slow or no internet connection.</p><p>Please check your internet settings</p></figcaption></figure>");
-});
